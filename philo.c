05f7221aea75dbc1fcc20760f717c077d6de091b
Shuga Agemura <sagemura@c1r11s1.42tokyo.jp>
2024-02-10 14:07:28 +0900

    update

--- philo.c
+++ philo.c
@@ -6,11 +6,36 @@
 /*   By: sagemura <sagemura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 02:11:44 by sagemura          #+#    #+#             */
-/*   Updated: 2024/02/10 04:18:18 by sagemura         ###   ########.fr       */
+/*   Updated: 2024/02/10 14:07:12 by sagemura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+int philo_allocation(void)
+{
+	t_vars	;
+}
+
+
+int	philo_set_vars(int ac, char **av)
+{
+	t_vars	vars;
+
+	vars.philo_num = ft_atoi(av[1]);
+	vars.time2die = ft_atoi(av[2]);
+	vars.time2eat = ft_atoi(av[3]);
+	vars.time2sleep = ft_atoi(av[4]);
+	if (ac == 6)
+		vars.philo_must_eat = ft_atoi(av[5]);
+	else
+		vars.philo_must_eat = -1;
+	if (vars.philo_num < 2 || vars.time2die < 0
+		|| vars.time2eat < 0 || vars.time2sleep < 0)
+		return (ft_error("Error: Invalid numbers"));
+	else
+		return (philo_allocation());
+}
 
 int	philo_ini(int ac, char **av)
 {
@@ -23,14 +48,15 @@ int	philo_ini(int ac, char **av)
 			return (ft_error("Error: Invalid numbers"));
 		i++;
 	}
-    
-    return(0);
+	if (philo_set_vars(ac, av) == -1)
+		return (ft_error("Error: Invalid numbers"));
+	return (philo_set_vars(ac, av));
 }
 
 int	main(int ac, char **av)
 {
 	if (ac > 4 && ac < 7)
-		return(philo_ini(ac, av));
+		return (philo_ini(ac, av));
 	else
 		return (ft_error("Error"));
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: sagemura <sagemura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 02:11:24 by sagemura          #+#    #+#             */
-/*   Updated: 2024/02/10 03:16:05 by sagemura         ###   ########.fr       */
+/*   Updated: 2024/02/10 14:02:59 by sagemura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,34 @@
 # define PHILO_H_
 
 # include <limits.h>
+# include <pthread.h>
 # include <stdio.h>
 # include <unistd.h>
 
-//helper_func
-int	ft_atoi(char *s);
-int	ft_error(char *s);
-size_t ft_strlen(char *s);
+typedef struct s_philo_vars
+{
+	int				diet_times;
+	int				pos;
+	int				ffork;
+	int				sfork;
+	pthread_t		id;
+}					t_philo_vars;
+
+typedef struct s_vars
+{
+	int				philo_num;
+	int				time2die;
+	int				time2eat;
+	int				time2sleep;
+	int				philo_must_eat;
+	t_philo_vars	*philo_vars;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	meal;
+	pthread_mutex_t	print;
+}					t_vars;
+
+int					ft_atoi(char *s);
+int					ft_error(char *s);
+size_t				ft_strlen(char *s);
 
 #endif
